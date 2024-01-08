c474e14114ae32cfc10a208819d02f5224a753c2
HENRIQUE BATISTA <85231417+henbatista@users.noreply.github.com>
2024-01-07 22:51:46 -0300

    Update useApiUrl.ts

--- composables/useApiUrl.ts
+++ composables/useApiUrl.ts
@@ -1,16 +1,16 @@
-import { ref } from "vue";
+import { ref } from 'vue'
 
 export default function useApiUrl() {
-  const homolog = ref(true);
+  const homolog = ref(true)
 
   const getApiUrl = () => {
     return homolog.value
-      ? "http://back.brt-repasse.com.br"
-      : "https://back.brtoperadora.com.br";
-  };
+      ? 'https://back.brtoperadora.com.br'
+      : 'https://back.brtoperadora.com.br'
+  }
 
   return {
     getApiUrl,
-    homolog,
-  };
+    homolog
+  }
 }
